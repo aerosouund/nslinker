9b0fbbc46d89a725b4adac8b87ea41fa687598f1
Ammar Yasser <yourgitemail@example.com>
2023-09-26 21:00:07 +0300

    nslinker init

--- main.go
+++ main.go
@@ -1,3 +1,53 @@
 package main
 
-func main() {}
+import (
+	"context"
+	"fmt"
+
+	"github.com/containerd/containerd"
+	"github.com/containerd/containerd/cio"
+	"github.com/containerd/containerd/namespaces"
+)
+
+type ContainerInfo struct {
+	ID  string
+	PID uint32
+}
+
+func getRunningContainers() {
+	ctx := namespaces.WithNamespace(context.Background(), "moby")
+
+	client, err := containerd.New("/run/containerd/containerd.sock")
+	if err != nil {
+		fmt.Printf("Error connecting to Containerd: %v\n", err)
+		return
+	}
+	defer client.Close()
+	containers, err := client.Containers(ctx)
+	if err != nil {
+		fmt.Printf("Error listing containers: %v\n", err)
+		return
+	}
+
+	var containerInfo []ContainerInfo
+
+	for _, container := range containers {
+		info, err := container.Info(ctx)
+		if err != nil {
+			fmt.Printf("Error getting container info: %v\n", err)
+			continue
+		}
+
+		task, err := container.Task(ctx, cio.NewAttach())
+		containerInfo = append(containerInfo, ContainerInfo{
+			ID:  info.ID,
+			PID: task.Pid(),
+		})
+
+		fmt.Printf("Container ID: %s. PID: %d\n", info.ID, task.Pid())
+	}
+}
+
+func main() {
+	getRunningContainers()
+}
